ec42799919c1e418521e10d0951f343a296a8e89
Michał Sikorski <sikorhurricane@gmail.com>
2018-03-09 12:29:02 +0100

    Add button to initiate a new game

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-var list = document.getElementById('list'),
-	add = document.getElementById('addElem');
-	elementsList = document.getElementsByTagName('li').length;
-	n = 1;
+var newGameBtn = document.getElementById('js-newGameButton');
 
-add.addEventListener('click', function() {
-	var element = document.createElement('li');
-
-	element.innerHTML = 'item ' + n++;
-
-	list.appendChild(element);
-
-	console.log(element);
-});
+newGameBtn.addEventListener('click', newGame);
