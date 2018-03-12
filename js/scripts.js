1098462bd905d85e8c91b22ad5a90b4afba1d841
Michał Sikorski <sikorhurricane@gmail.com>
2018-03-12 10:24:27 +0100

    Add displaying game elements

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,37 @@ var pickRock = document.getElementById('js-playerPick_rock'),
 pickRock.addEventListener('click', function() { playerPick('rock') });
 pickPaper.addEventListener('click', function() { playerPick('paper') });
 pickScissors.addEventListener('click', function() { playerPick('scissors') });
+
+var gameState = 'notStarted', //started //ended
+	player = {
+		name: '',
+		score: 0
+	},
+	computer = {
+		score: 0
+	};
+
+var newGameElem = document.getElementById('js-newGameElement'),
+	pickElem = document.getElementById('js-playerPickElement'),
+	resultsElem = document.getElementById('js-resultsTableElement');
+
+function setGameElements() {
+	switch(gameState) {
+		case 'started':
+			newGameElem.style.display = 'none';
+			pickElem.style.display = 'block';
+			resultsElem.style.display = 'block';
+		break;
+
+		case 'ended':
+			newGameBtn.innerText = 'Jeszcze raz';
+
+		case 'notStarted':
+		default:
+			newGameElem.style.display = 'block';
+			pickElem.style.display = 'none';
+			resultsElem.style.display = 'none';
+	}
+}
+
+setGameElements();
